3ff303cbe0f92e2d9751be250836983476f87c5f
Garrett Lander <garrett.jlander@gmail.com>
2025-02-10 17:30:07 +0100

    preserved original chat and made second to demo markdown

--- module06_gen_ai/sse-vanilla-to-react/sse-react-refactor/src/App.jsx
+++ module06_gen_ai/sse-vanilla-to-react/sse-react-refactor/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import Form from './components/Form';
 import Chat from './components/Chat';
+import ChatMarkdown from './components/ChatMarkdown';
 function App() {
     const [dataResult, setDataResult] = useState('');
     return (
         <div className='h-screen container mx-auto p-5 flex flex-col justify-between gap-5'>
             <Form setDataResult={setDataResult} />
             <Chat dataResult={dataResult} />
+            <ChatMarkdown dataResult={dataResult} />
         </div>
     );
 }
